a3957b090663ef3c1c858300356e6d06577663fa
Maciej Grzywocz <grzywocz.maciej@gmail.com>
2022-03-06 19:27:06 +0000

    Get initial status of gpios

--- src/webgpio/app_gpio.py
+++ src/webgpio/app_gpio.py
@@ -2,7 +2,7 @@
 
 import atexit
 import time
-from bottle import abort, post, request, route, run, static_file
+from bottle import abort, get, post, request, route, run, static_file
 import RPi.GPIO as g
 
 IN1 = 17  # RPi gpio for relay #1
@@ -10,9 +10,18 @@ IN2 = 27
 ACTIVE = g.LOW
 INACTIVE = g.HIGH
 
+def gpio_status():
+    status = str(g.input(IN1)) + str(g.input(IN2))
+    print(status)
+    return status + "\n"
+
 @route('/gpio')
 def gpio_static():
     return static_file('index.html', root='.')
+
+@route('/gpio/get')
+def gpio_get():
+    return gpio_status()
 
 @post('/gpio/set')
 def gpio_set():
@@ -20,18 +29,15 @@ def gpio_set():
     print(f'action={action}')
     if action == 'debug':
         g.output(IN1, INACTIVE)
-        ret = g.input(IN1)
     elif action == 'bypass':
         g.output(IN1, ACTIVE)
-        ret = g.input(IN1)
     elif action == 'reset':
         g.output(IN2, ACTIVE)
         time.sleep(0.5)
         g.output(IN2, INACTIVE)
-        ret = g.input(IN2)
     else:
         abort(400, "Invalid argument")
-    return str(ret) + "\n"
+    return gpio_status()
 
 g.setmode(g.BCM)
 g.setup([IN1, IN2], g.OUT, initial=g.HIGH)  # relay is active-low
